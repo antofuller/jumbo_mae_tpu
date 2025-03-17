8c7b170915151c9717c16b131446705fea6e9c6a
Anthony Fuller <anthonydavidfuller@gmail.com>
2025-03-17 12:38:59 -0400

    Update modeling.py

--- src/modeling.py
+++ src/modeling.py
@@ -111,10 +111,6 @@ class PatchEmbed(ViTBase, nn.Module):
             strides=(self.patch_size, self.patch_size),
             padding="VALID",
         )
-        if self.pooling == "cls":
-            self.cls_token = self.param(
-                "cls_token", init.truncated_normal(0.02), (1, 1, self.dim)
-            )
 
         if self.posemb == "learnable":
             self.wpe = self.param(
@@ -125,9 +121,6 @@ class PatchEmbed(ViTBase, nn.Module):
 
     def __call__(self, x: Array) -> Array:
         x = (self.wte(x) + self.wpe).reshape(x.shape[0], -1, self.dim)
-        if self.pooling == "cls":
-            cls_token = jnp.repeat(self.cls_token, x.shape[0], axis=0)
-            x = jnp.concatenate((cls_token, x), axis=1)
         return x
 
 
@@ -156,92 +149,133 @@ class FeedForward(ViTBase, nn.Module):
 
 
 class ViTLayer(ViTBase, nn.Module):
+    jumbo_mlp: nn.Module  # Shared jumbo MLP (wider MLP)
+    num_cls_tokens: int
+
     def setup(self):
         self.attn = Attention(**self.kwargs)
         self.ff = FeedForward(**self.kwargs)
 
         self.norm1 = nn.LayerNorm()
         self.norm2 = nn.LayerNorm()
-        self.drop = nn.Dropout(self.droppath, broadcast_dims=(1, 2))
+        self.norm3 = nn.LayerNorm()
 
-        self.scale1 = self.scale2 = 1.0
+        self.drop_path1 = nn.Dropout(self.droppath, broadcast_dims=(1, 2))
+        self.drop_path2 = nn.Dropout(self.droppath, broadcast_dims=(1, 2))
+        self.drop_path3 = nn.Dropout(self.droppath, broadcast_dims=(1,))
+
+        self.scale1 = self.scale2 = self.scale3 = 1.0
         if self.layerscale:
             self.scale1 = self.param("scale1", init.constant(1e-4), (self.dim,))
             self.scale2 = self.param("scale2", init.constant(1e-4), (self.dim,))
+            self.scale3 = self.param("scale3", init.constant(1e-4), (self.dim * self.num_cls_tokens,))
 
     def __call__(self, x: Array, det: bool = True) -> Array:
-        x = x + self.drop(self.scale1 * self.attn(self.norm1(x), det), det)
-        x = x + self.drop(self.scale2 * self.ff(self.norm2(x), det), det)
+        x = x + self.drop_path1(self.scale1 * self.attn(self.norm1(x), det), det)
+
+        cls_tokens, patch_tokens = x[:, :self.num_cls_tokens, :], x[:, self.num_cls_tokens:, :]
+
+        bs = cls_tokens.shape[0]
+        cls_tokens_concat = cls_tokens.reshape(bs, -1)
+        cls_tokens_concat = self.norm3(cls_tokens_concat)
+        cls_tokens_concat = cls_tokens_concat + self.drop_path3(self.scale3 * self.jumbo_mlp(cls_tokens_concat, det), det)
+
+        patch_tokens = patch_tokens + self.drop_path2(self.scale2 * self.ff(self.norm2(patch_tokens), det), det)
+
+        cls_tokens = cls_tokens_concat.reshape(bs, self.num_cls_tokens, -1)
+        x = jnp.concatenate([cls_tokens, patch_tokens], axis=1)
+
         return x
 
 
 class LinearCLS(nn.Module):
     labels: int
     batch_norm: bool
-    
+    dim: int
+
     @nn.compact
     def __call__(self, x: Array, det: bool) -> Array:
         if self.batch_norm:
             x = nn.BatchNorm(use_running_average=det, axis_name="batch")(x)
-        x = nn.Dense(self.labels, kernel_init=init.truncated_normal(0.02))(x)
+        x = nn.Dense(self.labels, kernel_init=init.truncated_normal(0.02), use_bias=True)(x)
         return x
 
 class ViT(ViTBase, nn.Module):
+    num_cls_tokens: int = 1
+
     def setup(self):
         self.embed = PatchEmbed(**self.kwargs)
         self.drop = nn.Dropout(self.dropout)
 
-        # The layer class should be wrapped with `nn.remat` if `grad_ckpt` is enabled.
+        jumbo_kwargs = dict(self.kwargs)
+        jumbo_kwargs['hidden_dim'] = self.hidden_dim * self.num_cls_tokens
+        jumbo_kwargs['dim'] = self.dim * self.num_cls_tokens
+        self.jumbo_mlp = FeedForward(**jumbo_kwargs)
+
         layer_fn = nn.remat(ViTLayer) if self.grad_ckpt else ViTLayer
-        self.layer = [layer_fn(**self.kwargs) for _ in range(self.layers)]
+        self.layer = [
+            layer_fn(jumbo_mlp=self.jumbo_mlp, num_cls_tokens=self.num_cls_tokens, **self.kwargs)
+            for _ in range(self.layers)
+        ]
 
         self.norm = nn.LayerNorm()
-        
-        # head
-        self.head = LinearCLS(self.labels, self.batch_norm) if self.labels > 0 else None
+        self.head = (
+            LinearCLS(self.labels, self.batch_norm, dim=self.dim * self.num_cls_tokens)
+            if self.labels > 0 else None
+        )
+        self.cls_tokens = self.param("cls_tokens", init.zeros, (1, self.num_cls_tokens, self.dim))
 
     def __call__(self, x: Array, det: bool = True) -> Array:
         x = self.embed(x)
-        if self.head is None: # MAE + Training: Perturb PatchEmbeds, forward_encoder
-            cls_token, x = x[:, :1, :], x[:, 1:, :] # x[:, 0, :] decreases dimension
-            bs, seq_len, dim = x.shape
+        bs = x.shape[0]
+
+        cls_tokens = jnp.tile(self.cls_tokens, (bs, 1, 1))
+        x = jnp.concatenate((cls_tokens, x), axis=1)
+
+        if self.head is None:  # MAE mode
+            cls_tokens, patch_tokens = x[:, :self.num_cls_tokens, :], x[:, self.num_cls_tokens:, :]
+            seq_len = patch_tokens.shape[1]
             image_keep_length = int(seq_len * (1.0 - self.image_mask_ratio))
-            x, image_mask, image_ids_restore = random_masking(x, self.make_rng("noise"), image_keep_length)
-            x = jnp.concatenate((cls_token, x), axis=1)
+            patch_tokens, image_mask, image_ids_restore = random_masking(patch_tokens, self.make_rng("noise"), image_keep_length)
+            x = jnp.concatenate((cls_tokens, patch_tokens), axis=1)
+
         x = self.drop(x, det)
+
         for layer in self.layer:
             x = layer(x, det)
+
         x = self.norm(x)
 
-        # If the classification head is not defined, then return the output of all
-        # tokens instead of pooling to a single vector and then calculate class logits.
-        if self.head is None: # MAE for both training and inference, forward_encoder
+        if self.head is None:  # MAE mode
             return x, image_mask, image_ids_restore
 
         if self.linear_probing:
             x = jax.lax.stop_gradient(x)
 
-        if self.pooling == "cls":
-            x = x[:, 0, :]
-        elif self.pooling == "gap":
-            x = x.mean(1)
+        x = x[:, :self.num_cls_tokens, :].reshape(bs, -1)  # concatenate CLS tokens
         
         return self.head(x, det)
 
-class MAEDecoder(MAEDecoderBase, nn.Module):
+class MAEDecoder(MAEBase, nn.Module):
+    num_cls_tokens: int = 1
+
     def setup(self):
         layer_fn = nn.remat(ViTLayer) if self.grad_ckpt else ViTLayer
         self.dec_layer = [layer_fn(**self.kwargs) for _ in range(self.dec_layers)]
         self.dec_norm = nn.LayerNorm()
 
         self.dec_wpe = fixed_sincos2d_embeddings(*self.num_patches, self.dec_dim)
-    
+
     def __call__(self, x: Array, det: bool = True) -> Array:
-        cls_token, x = x[:, :1, :], x[:, 1:, :]
-        x = x + self.dec_wpe.reshape(-1, self.dec_dim)
-        x = jnp.concatenate((cls_token, x), axis=1)
+        bs = x.shape[0]
+
+        cls_tokens, patch_tokens = x[:, :self.num_cls_tokens, :], x[:, self.num_cls_tokens:, :]
+        patch_tokens = patch_tokens + self.dec_wpe.reshape(1, -1, patch_tokens.shape[-1])
+
+        x = jnp.concatenate((cls_tokens, patch_tokens), axis=1)
 
         for layer in self.dec_layer:
             x = layer(x, det)
+
         x = self.dec_norm(x)
         return x
