c437c44486bacc50a7f3aa7beda54e89782d4fae
Anthony Fuller <anthonydavidfuller@gmail.com>
2025-11-13 16:43:03 -0500

    Update ft.sh

--- config/ft.sh
+++ config/ft.sh
@@ -37,8 +37,8 @@ python3 src/main_finetune.py \
     --lr-decay 0.75 \
     --clip-grad 0.0 \
     --grad-accum 1 \
-    --warmup-steps $((1281167 * 20 / 1024)) \
-    --training-steps $((1281167 * 120 / 1024)) \
+    --warmup-steps $((1281167 * 10 / 1024)) \
+    --training-steps $((1281167 * 110 / 1024)) \
     --log-interval 10 \
     --eval-interval $((1281167 * 1 / 1024)) \
     --project MAE-JAX \
